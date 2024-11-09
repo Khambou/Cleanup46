f6895d61695b5135600b0d1d6e1662f718659efb
Dytoma Batogouma <dytomaomar@gmail.com>
2024-11-09 15:10:34 +0100

    :white_check_mark: fix declarations

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
-import Nav from './components/navbar';
-import HomePage from './components/home';
-import PricesPage from './components/prices';
+import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Navbar.jsx";
+import HomePage from "./pages/home.jsx";
+import PricesPage from "./pages/prices.jsx";
+import AboutPage from "./pages/about-us.jsx";
+import ContactPage from "./pages/contact-us.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Nav />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/tarifs" element={<PricesPage />}/>
+        <Route path="/tarifs" element={<PricesPage />} />
+        <Route path="/about-us" element={<AboutPage />} />
+        <Route path="/contact-us" element={<ContactPage />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
 
