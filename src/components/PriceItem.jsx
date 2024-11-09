a7437aeab6615a78a7fe6a3c4cc60aec2d72cef0
Dytoma Batogouma <dytomaomar@gmail.com>
2024-11-09 21:08:47 +0100

    :white_check_mark: add PriceItem component

---
+++ src/components/PriceItem.jsx
@@ -0,0 +1,28 @@
+import PriceTag from "./PriceTag";
+import { Check } from "../assets/icons";
+
+const PriceItem = ({ priceItem }) => {
+  const { title, price, benefits, priceStyle, bgColor } = priceItem;
+  return (
+    <div
+      className={`p-8 flex-layout--center flex-col gap-12 rounded-3xl shadow-drop w-[25.5rem] ${bgColor ? bgColor : ""}`}
+    >
+      <div className="flex-layout--center flex-col gap-8">
+        <h3 className="text-h2">{title}</h3>
+        <PriceTag price={price} priceStyle={priceStyle} />
+      </div>
+      <div className="flex flex-col items-start gap-3">
+        {benefits.map((benefit) => (
+          <div key={benefit} className="flex-layout--center gap-5">
+            <div className="font-dm-sans py-[5px] px-[4.38px] rounded-full bg-sky-blue-color">
+              <img src={Check} alt="Check Icon" width={10} height={7.98} />
+            </div>
+            <p className="text-base font-dm-sans">{benefit}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PriceItem;
