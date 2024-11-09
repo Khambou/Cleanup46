686876cc348b5660cf6ef4e14d984db08ea0da99
Dytoma Batogouma <dytomaomar@gmail.com>
2024-11-09 15:08:51 +0100

    :test_tube: modify the theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./public/index.html",
-  ],
-  theme: {},
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./public/index.html"],
+  theme: {
+    colors: {
+      primary: "#00A1F2",
+      "text-primary": "#374955",
+      "green-color": "#39C08F",
+      secondary: "#F2F2F2",
+      "red-color": "#FA6A72",
+      "sky-blue-color": "#F0FBFF",
+    },
+    fontFamily: {
+      "dm-sans": ["DM Sans", "sans-serif"],
+      grotesque: ["Bricolage Grotesque", "sans-serif"],
+    },
+    fontSize: {
+      "0xl": ["5rem", "96px"],
+    },
+  },
   plugins: [],
-}
-
+};
