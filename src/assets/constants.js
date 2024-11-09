e2d3ca2accc89c8761ef52215775af77cc8d0c34
Dytoma Batogouma <dytomaomar@gmail.com>
2024-11-09 21:04:50 +0100

    :white_check_mark: add constants to the project

---
+++ src/assets/constants.js
@@ -0,0 +1,43 @@
+export const priceItems = [
+  {
+    title: "Formule standard",
+    price: 60,
+    benefits: [
+      "Aspiration complète de l'habitacle",
+      "Nettoyage de tous les plastiques intérieurs",
+      "Nettoyage des vitres",
+      "Aspiration du coffre",
+    ],
+  },
+  {
+    title: "Formule Premium",
+    price: 90,
+    benefits: [
+      "Aspiration en profondeur de l'habitacle",
+      "Nettoyage détaillé de tous les plastiques et aérations",
+      "Application d'une protection pour les plastiques",
+      "Pressing des sièges et nettoyage du cuir",
+      "Nettoyage complet du coffre",
+      "Nettoyage des joints de portes",
+      "Nettoyage des contours de portes",
+    ],
+  },
+  {
+    title: "Formule Prestige",
+    price: 150,
+    benefits: [
+      "Aspiration en profondeur de l'habitacle",
+      "Nettoyage complet des plastiques intérieurs",
+      "Nettoyage des vitres intérieures et extérieures",
+      "Pressing des sièges et nettoyage en profondeur du cuir",
+      "Nettoyage complet du coffre",
+      "Nettoyage des joints de portes",
+      "Nettoyage du ciel de toit",
+      "Nettoyage détaillé du compartiment de la roue de secours",
+      "Nettoyage des ceintures de sécurité",
+      "Nettoyage approfondi des contours de portes",
+    ],
+    priceStyle: "text-white-color",
+    bgColor: "bg-green-color",
+  },
+];
