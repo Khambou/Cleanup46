e62e59c19aa050fcca21b0bd161090e258ad84ba
Cédric Khambounheuang <cedric.khambounheuang@epitech.eu>
2024-11-09 13:47:31 +0100

    Fixing routes issues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Nav from './components/navbar';
 import HomePage from './components/home';
 import PricesPage from './components/prices';
@@ -8,13 +8,11 @@ function App() {
   return (
     <>
       <Nav />
-      <HashRouter basename="/">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/tarifs" element={<PricesPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
-      </HashRouter>
     </>
   );
 }
--- src/components/home.js
+++ src/components/home.js
@@ -5,7 +5,7 @@ function HomePage() {
         <div class="home">
             <div class="title">
                 <h1>Faites briller vos espaces de vie !</h1>
-                <img class="cleaning-car rounded" src="/img/cleaning-car.png" alt="cleaning" />
+                <img class="cleaning-car rounded" src={process.env.PUBLIC_URL + '/img/cleaning-car.png'} alt="cleaning" />
             </div>
         </div>
     );
