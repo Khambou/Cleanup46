5ae15d87f6bd337890e96a8741d8f973a35b4c34
Dytoma Batogouma <dytomaomar@gmail.com>
2024-11-09 21:06:48 +0100

    :alembic: simplify the exports of icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,47 @@
+import AddCircle from "./add-circle.svg";
+import ArrowDown from "./arrow-down.svg";
+import ArrowRight from "./arrow-right.svg";
+import ArrowUp from "./arrow-up.svg";
+import Call from "./call.svg";
+import CleaningHand from "./cleaning-hand.svg";
+import CustomerService from "./customer-service.svg";
+import Environment from "./environment.svg";
+import Export from "./export.svg";
+import Instagram from "./instagram.svg";
+import Location from "./location.svg";
+import MapFrame from "./map-frame.svg";
+import MinusCircle from "./minus-cirlce.svg";
+import People from "./people.svg";
+import Quality from "./quality.svg";
+import Reliabale from "./reliabale.svg";
+import Sms from "./sms.svg";
+import SofaCouch from "./sofa-couch.svg";
+import StarFast from "./star-fast.svg";
+import Trophy from "./trophy.svg";
+import Whatsapp from "./whatsapp.svg";
+import Check from "./check-lg.svg";
+
+export {
+  AddCircle,
+  ArrowDown,
+  ArrowRight,
+  ArrowUp,
+  Call,
+  CleaningHand,
+  CustomerService,
+  Environment,
+  Export,
+  Instagram,
+  Location,
+  MapFrame,
+  MinusCircle,
+  People,
+  Quality,
+  Reliabale,
+  Sms,
+  SofaCouch,
+  StarFast,
+  Trophy,
+  Whatsapp,
+  Check,
+};
