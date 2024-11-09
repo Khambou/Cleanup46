1ee135b8098a4bbc7ade6e1e0ae316760214b431
Dytoma Batogouma <dytomaomar@gmail.com>
2024-11-09 21:06:55 +0100

    :alembic: simplify the exports of images

---
+++ src/assets/images/index.js
@@ -0,0 +1,21 @@
+import PricingHeaderImg from "./car-clean-1.jpeg";
+import HomeHeroImg from "./men-cleaning.png";
+import PricingMiddleImg from "./cleaner-4.jpg";
+import AboutUsHeaderImg from "./car-team.jpg";
+import ImageGallery1 from "./man-washing-car.jpg";
+import ImageGallery2 from "./cleaner-1.jpg";
+import ImageGallery3 from "./cleaner-2.jpg";
+import ImageGallery4 from "./car-clean-3.jpg";
+import ImageGallery5 from "./car-clean-2.jpg";
+
+export {
+  PricingHeaderImg,
+  HomeHeroImg,
+  PricingMiddleImg,
+  AboutUsHeaderImg,
+  ImageGallery1,
+  ImageGallery2,
+  ImageGallery3,
+  ImageGallery4,
+  ImageGallery5,
+};
